935749afb449fc8d3e19eb390995c62f004710f7
Ashwani Garg <akgarg007@gmail.com>
2018-06-26 16:49:51 +0530

    Company link added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,16 +25,17 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Laravel CRM') }}
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
+                    <ul class="navbar-nav mr-auto push-right">
+                        <li><a href="/companies">Companies</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
